aa73c9174cf0c15508fa6db79abf9a3c1fe56a3f
bruno <batrillo@gmail.com>
2016-04-29 13:45:09 -0600

    yo

--- BandStandEntities/src/test/TestJunit.java
+++ BandStandEntities/src/test/TestJunit.java
@@ -1,5 +1,7 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -7,6 +9,10 @@ import javax.persistence.Persistence;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import entities.Artist;
+import entities.Booking;
+import entities.User;
 
 public class TestJunit {
 
@@ -22,8 +28,27 @@ public class TestJunit {
 	}
 
 	@Test
-	public void test() {
+	public void testArtist() {
 
+		Artist a = em.find(Artist.class, 1);
+		assertEquals("HankyPanky", a.getName());
+		assertEquals(1, a.getId());
+		assertEquals("Country", a.getGenres());
+
+	}
+
+	@Test
+	public void testUser() {
+
+		User u = em.find(User.class, 1);
+		assertEquals("Test", u.getFirstName());
+	}
+	
+	@Test
+	public void testBooking() {
+		
+		Booking b = em.find(Booking.class, 1);
+		
 	}
 
 	@After
