44760b70b09ec59f0c169cd5b23f77ec75f462c6
Kaylee McHugh <kayleemchugh@Kaylees-MacBook-Pro.local>
2016-04-29 08:43:49 -0600

    Genre class has been added

--- BandStandEntities/src/entities/Genre.java
+++ BandStandEntities/src/entities/Genre.java
@@ -1,6 +1,71 @@
 package entities;
 
-public enum Genre {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="GENRE")
+public class Genre {
 	
-Jazz, Classical, Reggae, Country, EDM, Brass, Alternative, Rock, Chamber, Bagpipe_Duo, Rap 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "artist_id")
+	private Artist artist;
+	
+	@Column(name="name")
+	@Enumerated(EnumType.STRING)
+	private GenreType genretype;
+	
+	public Genre() {
+		
+	}
+	
+	public Genre(int id, Artist artist, GenreType genretype) {
+		super();
+		this.id = id;
+		this.artist = artist;
+		this.genretype = genretype;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Artist getArtist() {
+		return artist;
+	}
+
+	public void setArtist(Artist artist) {
+		this.artist = artist;
+	}
+
+	public GenreType getGenretype() {
+		return genretype;
+	}
+
+	public void setGenretype(GenreType genretype) {
+		this.genretype = genretype;
+	}
+
+
+	
+	
+	
+
 }
---
+++ BandStandEntities/src/entities/GenreType.java
@@ -0,0 +1,6 @@
+package entities;
+
+public enum GenreType {
+	Jazz, Classical, Reggae, Country, EDM, Brass, Alternative, Rock, Chamber, Bagpipe_Duo, Rap
+	
+	}
